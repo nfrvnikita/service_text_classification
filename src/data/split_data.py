b86754240e21f54d275b4ba927d8218e53dc590a
nfrvnikita <nfrv.nikita@gmail.com>
2023-08-31 03:02:36 +0300

    debug, model weights e.t.c.

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -1,19 +1,21 @@
 # pylint: disable=invalid-name
 """Импорт библиотек"""
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 
-def split(data_os=pd.DataFrame, seed=42):
+PATH_TO_SAVE = '/home/nfrvnikita/projects/service4classification/data/interim/'
+
+
+def split_dataset(data=pd.DataFrame, train_ratio=0.85):
     """_summary_
 
     Args:
-        data_os (_type_, optional): _description_. Defaults to pd.DataFrame.
+        data (_type_, optional): _description_. Defaults to pd.DataFrame.
+        train_ratio (float, optional): _description_. Defaults to 0.85.
     """
-    X = data_os['clean_review'].values
-    y = data_os['class'].values
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y,
-                                                          test_size=0.25,
-                                                          stratify=y,
-                                                          random_state=seed)
-    return X_train, X_valid, y_train, y_valid
+    train_size = int(len(data) * train_ratio)
+    train_set = data[:train_size]
+    test_set = data[train_size:]
+
+    train_set.to_csv(f'{PATH_TO_SAVE}train_set.csv', index=False)
+    test_set.to_csv(f'{PATH_TO_SAVE}test_set.csv', index=False)
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import streamlit as st
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 def count_words_review(data=pd.DataFrame):
@@ -18,3 +20,25 @@ def count_words_review(data=pd.DataFrame):
     plt.ylabel('Кол-во')
     plt.xlabel('')
     plt.show()
+
+
+def plot_metrics(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    f1 = f1_score(y_true, y_pred, average='weighted')
+
+    metric_dict = {
+        'Accuracy': accuracy,
+        'Precision': precision,
+        'Recall': recall,
+        'F1-Score': f1
+    }
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=list(metric_dict.keys()), y=list(metric_dict.values()))
+    plt.title('Model Performance Metrics')
+    plt.xlabel('Metrics')
+    plt.ylabel('Score')
+    plt.ylim(0, 1)
+    st.pyplot(plt)
